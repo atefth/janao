95a6d77466b342527886b280af7abef398deb2f1
Irfan <irfandhk@gmail.com>
2018-07-11 15:54:03 +0600

    Update documentation for Janao Postman

--- lib/janao/services/postman.rb
+++ lib/janao/services/postman.rb
@@ -15,73 +15,83 @@ module Janao
     
     extend self
     
-    # For configuration object
-    Configuration = Class.new(ActiveSupport::OrderedOptions)
+    # Sets configuration object for various adapter
+    #
+    class Configuration < ActiveSupport::OrderedOptions; end
     
-    # Errors to be used
-    EmailNotValid                = Class.new(StandardError)
-    AdapterNotValid              = Class.new(StandardError)
-    ToEmailNotProvided           = Class.new(ArgumentError)
-    AdapterNotSpecified          = Class.new(StandardError)
-    PostmanMethodNotImplemented  = Class.new(NotImplementedError)
-    SmsOrEmailShouldBeSwitchedOn = Class.new(StandardError)
-    EmailTemplateNameNotProvided = Class.new(StandardError)
+    # Error classes to be used
+    #
+    class EmailNotValid                < StandardError; end # Raise when Email address not valid
+    class AdapterNotValid              < StandardError; end # Raise when Adapter not configured from the valid list
+    class ToEmailNotProvided           < StandardError; end # Raise when Email address not provided
+    class AdapterNotSpecified          < StandardError; end # Raise when Adapter not specified
+    class PostmanMethodNotImplemented  < NotImplementedError; end # Raise when expected method not implemented
+    class SmsOrEmailShouldBeSwitchedOn < StandardError; end # Raise when both sms and email is disabled from the configuration.
+    class EmailTemplateNameNotProvided < StandardError; end
     
+    # Sets if engine can also send emails
     mattr_accessor :send_email, default: true
     
-    # Adapter to be set!
+    # Sets email adapter to be used, Default: 'MailGun'
     mattr_accessor :email_adapter, default: 'MailGun'
     
+    # Sets sms adapter, Default: 'Custom'
     mattr_accessor :sms_adapter, default: 'Custom'
     
-    # Should postman send sms as well?
+    # Sets configuration that allow engine to send sms as well
     mattr_accessor :send_sms, default: true
     
-    # Sms length should be
+    # Sets sms length to be validated dynamically
     mattr_accessor :sms_length, default: 160
     
-    # Adapter related configurations to be set, Default: Configuration.new
+    # Sets adapter related configurations to be set, Default: Configuration.new
     mattr_accessor :mailer_configuration, default: Configuration.new
     
-    # Configuration for SMS Configuration
+    # Sets configuration for SMS
     mattr_accessor :sms_configuration, default: Configuration.new
     
-    # Dynamic token validation for email template, Default: true
+    # Sets dynamic token validation for email template, Default: true
     mattr_accessor :validate_template_token, default: true
     
-    # Template related settings
+    # Sets template related settings
     mattr_accessor :common_footer, default: nil
     
+    # Sets common_header, default: nil
     mattr_accessor :common_header, default: nil
     
-    # Dynamically convert date format
+    # Sets date format dynamically
     mattr_accessor :date_format, default: '%d-%m-%Y'
     
+    # Sets signature, default: nil
     mattr_accessor :signature, default: nil
     
+    # Sets copyright_message, default: nil
     mattr_accessor :copyright_message, default: nil
     
+    # Sets company_title, default: nil
     mattr_accessor :company_title, default: nil
     
     # Config Janao::Postman to send email and sms notifications
     # Alias method is .setup of .config, you can use both
     #
     # Example:
-    #   Janao::Postman.setup do |config|
-    #     # Use only to send email?
-    #     config.send_email                   = true
-    #     config.send_sms                     = true
-    #     config.email_adapter                = 'MailGun'
-    #     config.sms_adapter                  = 'Custom'
-    #     # Optional parameters
-    #     config.mailer_configuration.key     = 'Some sort of identification key'
-    #     config.mailer_configuration.secret  = 'Don\'t disclose your secret'
-    #     config.sms_configuration.username   = 'iirfann'
-    #     config.sms_configuration.password   = 'Passw0rd'
-    #     config.common_header                = "<h2>Hay Man</h2>"
-    #     config.copyright_message            = "Copyright at Your Company @ 2018"
-    #   end
-    
+    #       Janao::Postman.setup do |config|
+    #         # Use only to send email?
+    #         config.send_email                   = true
+    #         config.send_sms                     = true
+    #         config.email_adapter                = 'MailGun'
+    #         config.sms_adapter                  = 'Custom'
+    #         # Optional parameters
+    #         config.mailer_configuration.key     = 'Some sort of identification key'
+    #         config.mailer_configuration.secret  = 'Don\'t disclose your secret'
+    #         config.sms_configuration.username   = 'iirfann'
+    #         config.sms_configuration.password   = 'Passw0rd'
+    #         config.common_header                = "<h2>Hay Man</h2>"
+    #         config.copyright_message            = "Copyright at Your Company @ 2018"
+    #       end
+    #
+    #
+    # @return [Janao::Postman] instance
     def config
       yield self
     end
@@ -91,14 +101,32 @@ module Janao
     # Just use .() to Send notifications!
     # .()
     #
-    # @param[Hash] emails to send emails
-    # @param[Array] emails[:to] collection of emails to be used in to
-    # @param[Array] emails[:cc] collection of emails to be used in cc
-    # @param[Array] emails[:bcc] collection of emails to be used in bcc
-    # @param[String] email_template_name to determine the template of email
-    # @param[Hash] sms_data to send sms
-    # @param[Array] numbers to be sent in sms
-    # @param[String] sms_template_name to determine the sms template
+    # The gateway to send message is to call this method
+    #
+    #   Janao::Postman.({
+    #     email: {
+    #       to: ['irfandhk@gmail.com', 'atef@hashkloud.com']
+    #     },
+    #     email_template_name: 'registration_greetings',
+    #     email_data: { full_name: 'Irfan Ahmed', tnx_number: 'HK29874297492842' },
+    #     sms_data: { full_name: 'Irfan Ahmed', tnx_number: 'HK29874297492842' },
+    #     numbers: ['+8801766678130', '+880197767130'],
+    #     sms_template_name: 'greet_new_user'
+    #   })
+    #
+    # @overload method(options)
+    #   @param [Array] args
+    #   @option options [String] :subject email's subject.
+    #   @option options [Hash] :email that takes to, cc and bcc keys witht the value of array type
+    #   @option options [String] :email_template_name to load the expected template provided by client
+    #   @option options [Hash] :email_data that contains token values of email template
+    #   @option options [Boolean] :send_email should be true by configuration
+    #   @option options [Boolean] :send_sms should be true or false by configuration
+    #   @option options [Hash] :sms_data should contain token values for sms template's token
+    #   @option options [Array] :numbers should represents list of numbers sms to be sent
+    #   @option options [String] :sms_template_name should represent the sms text template
+    #
+    # @return [Janao::Postman::Result] object that contains messages, error (true/false), backtrace: []
     def call(*args)
       options = args.extract_options!
       
